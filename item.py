64536fcf1b975a0c0711a7cf42639b1aa1e792ff
Bruce Neco <bruce.neco3@outlook.com>
2021-05-15 16:30:32 -0300

    Add docstrings to methods and classes

--- item.py
+++ item.py
@@ -4,11 +4,20 @@ from flask_jwt import jwt_required
 
 
 class Item(Resource):
+    """Representation of a store item."""
     parser = reqparse.RequestParser()
     parser.add_argument("price", type=float, required=True, help="This field cannot be left empty!")
 
     @jwt_required()
     def get(self, name):
+        """
+        Gets item from database with name passed in argument.
+
+        Parameters:
+            name(str): name of the item.
+        Returns:
+             Dict with item, or dict with message.
+        """
         connection = sqlite3.connect("data.db")
         cursor = connection.cursor()
 
@@ -22,6 +31,14 @@ class Item(Resource):
         return {"message": "Item not found."}, 404
 
     def post(self, name):
+        """
+        Creates item with passed name and price in request body.
+
+        Parameters:
+            name(str): name of the item to be created;
+        Return:
+            dict with item created or dict with message.
+        """
         if next(filter(lambda x: x["name"] == name, items), None):
             return {"message": f"An item with name {name} aready exists."}, 400
         req_data = self.parser.parse_args()
@@ -30,11 +47,28 @@ class Item(Resource):
         return item, 201
 
     def delete(self, name):
+        """
+        Removes item with passed name from database.
+
+        Parameters:
+            name(str): name of the item to be deleted.
+        Return:
+            dict with deletion message.
+        """
         global items
         items = next(filter(lambda x: x["name"] != name, items), None)
         return {"message": "Item deleted"}
 
     def put(self, name):
+        """
+        Updates item if already exists or create on if not exists.
+        In both cases, is neccessary to have a request body with price.
+
+        Parameters:
+            name(str): name of the item to be created/updated.
+        Returns:
+            dict of item created or updated.
+        """
         item = next(filter(lambda x: x["name"] == name, items), None)
         req_data = self.parser.parse_args()
         if item:
@@ -46,5 +80,13 @@ class Item(Resource):
 
 
 class ItemList(Resource):
+    """Representation of whole collection of items."""
+
     def get(self):
+        """
+        Gets all the items from database.
+
+        Returns:
+            dict with items inside a list.
+        """
         return {"items": items}
--- user.py
+++ user.py
@@ -3,6 +3,8 @@ from flask_restful import Resource, reqparse
 
 
 class User:
+    """User entity representation."""
+
     def __init__(self, _id, username, password):
         self.id = _id
         self.username = username
@@ -10,6 +12,14 @@ class User:
 
     @classmethod
     def find_by_username(cls, username):
+        """
+        Gets user using its unique username.
+
+        Parameters:
+            username(str): unique username to be searched
+        Returns:
+            User instance of found user, or None if nothing was find.
+        """
         connection = sqlite3.connect("data.db")
         cursor = connection.cursor()
 
@@ -25,6 +35,14 @@ class User:
 
     @classmethod
     def find_by_id(cls, _id):
+        """
+        Gets user using its unique username.
+
+        Parameters:
+            _id(int): unique id to be searched
+        Returns:
+            User instance of found user, or None if nothing was find.
+        """
         connection = sqlite3.connect("data.db")
         cursor = connection.cursor()
 
@@ -40,11 +58,18 @@ class User:
 
 
 class UserRegister(Resource):
+    """User registration resource."""
     parser = reqparse.RequestParser()
     parser.add_argument(name="username", required=True, type=str, help="Username field must me filled.")
     parser.add_argument(name="password", required=True, type=str, help="Password field must me filled.")
 
     def post(self):
+        """
+        Creates a user with data passed in request body.
+
+        Returns:
+            dict with message if user was created or if username already exists.
+        """
         data = self.parser.parse_args()
 
         if User.find_by_username(data["username"]):
