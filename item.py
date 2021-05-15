767a4ae365a9564e6a5b062f1a81115aa4342acc
Bruce Neco <bruce.neco3@outlook.com>
2021-05-15 17:14:13 -0300

    Adjust items methods to access database

--- item.py
+++ item.py
@@ -18,6 +18,21 @@ class Item(Resource):
         Returns:
              Dict with item, or dict with message.
         """
+        item = self.get_by_name(name)
+        if item:
+            return item
+        return {"message": "Item not found."}, 404
+
+    @classmethod
+    def get_by_name(cls, name):
+        """
+        Gets item from database with name passed in argument.
+
+        Parameters:
+            name(str): name of the item.
+        Returns:
+             Dict with item, or None.
+        """
         connection = sqlite3.connect("data.db")
         cursor = connection.cursor()
 
@@ -27,8 +42,8 @@ class Item(Resource):
         connection.close()
 
         if item:
-            return {"item": {"name": item[0], "price": item[1]}}
-        return {"message": "Item not found."}, 404
+            return {"name": item[0], "price": item[1]}
+        return None
 
     def post(self, name):
         """
@@ -39,12 +54,36 @@ class Item(Resource):
         Return:
             dict with item created or dict with message.
         """
-        if next(filter(lambda x: x["name"] == name, items), None):
-            return {"message": f"An item with name {name} aready exists."}, 400
         req_data = self.parser.parse_args()
-        item = {"name": name, "price": req_data["price"]}
-        items.append(item)
+        item = self.create_item(name, req_data["price"])
+
+        if not item:
+            return {"message": f"An item with name {name} aready exists."}, 400
         return item, 201
+
+    @classmethod
+    def create_item(cls, name, price):
+        """
+        Creates item with name and price passed.
+
+        Parameters:
+            name(str): name of the item
+        Returns:
+            Dict with item or None.
+        """
+        if cls.get_by_name(name):
+            return None
+
+        item = {"name": name, "price": price}
+
+        connection = sqlite3.connect("data.db")
+        cursor = connection.cursor()
+
+        cursor.execute("INSERT INTO items VALUES (?, ?)", (item["name"], item["price"]))
+
+        connection.commit()
+        connection.close()
+        return item
 
     def delete(self, name):
         """
@@ -55,8 +94,14 @@ class Item(Resource):
         Return:
             dict with deletion message.
         """
-        global items
-        items = next(filter(lambda x: x["name"] != name, items), None)
+        connection = sqlite3.connect("data.db")
+        cursor = connection.cursor()
+
+        cursor.execute("DELETE FROM items WHERE name=?", (name,))
+
+        connection.commit()
+        connection.close()
+
         return {"message": "Item deleted"}
 
     def put(self, name):
@@ -69,13 +114,18 @@ class Item(Resource):
         Returns:
             dict of item created or updated.
         """
-        item = next(filter(lambda x: x["name"] == name, items), None)
         req_data = self.parser.parse_args()
-        if item:
-            item.update(req_data)
-        else:
+        item = self.create_item(name, req_data["price"])
+        if not item:
+            connection = sqlite3.connect("data.db")
+            cursor = connection.cursor()
+
+            cursor.execute("UPDATE items SET price=? WHERE name=?", (req_data["price"], name))
+
+            connection.commit()
+            connection.close()
             item = {"name": name, "price": req_data["price"]}
-            items.append(item)
+
         return item
 
 
@@ -89,4 +139,13 @@ class ItemList(Resource):
         Returns:
             dict with items inside a list.
         """
+        connection = sqlite3.connect("data.db")
+        cursor = connection.cursor()
+
+        items = cursor.execute("SELECT * FROM items")
+
+        connection.close()
+
+        items = [{"name": item[0], "price": item[1]} for item in items]
+
         return {"items": items}
